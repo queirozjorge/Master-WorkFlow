27639c0136146f0fc497755c80d46f1c09a1e6c0
Jorge <jorge-rick@hotmail.com>
2022-05-12 21:50:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/masterworkflow/MasterWorkflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.masterworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/masterworkflow/MasterWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.masterworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

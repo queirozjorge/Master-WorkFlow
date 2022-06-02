fe3d228c7ae6916c5e5b0b07fbdfd4132fa5d69f
Jorge <jorge-rick@hotmail.com>
2022-06-02 18:24:54 -0300

    Modelando Entity de Defeito

---
+++ src/main/java/br/com/masterworkflow/model/Defeito.java
@@ -0,0 +1,49 @@
+package br.com.masterworkflow.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Defeito {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String texto;
+	
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Unidade unidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
